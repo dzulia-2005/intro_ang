29a13b1352d2326ca3967ebde4af73afb5060c24
dzulia-2005 <159149786+dzulia-2005@users.noreply.github.com>
2025-03-29 14:38:24 +0400

    first commit

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, Input, Output,  } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  @Input({required:true}) user!: {
+    id:string;
+    name:string;
+    avatar:string;
+  }
+  @Output() select = new EventEmitter();
+
+  get imagePath() {
+    return `assets/users/${this.user.avatar}`
+  }
+
+  onSelectUser () {
+    this.select.emit(this.user.id)
+  }
+}
