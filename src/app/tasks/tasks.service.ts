765d0ccddea7b6a43a46c0e82373696f41fccf92
dzulia-2005 <159149786+dzulia-2005@users.noreply.github.com>
2025-04-02 18:12:01 +0400

    added localstorage when i added task or delete and complete

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -36,8 +36,16 @@ export class TaskService {
     }
   ]
 
+  constructor(){
+    const tasks = localStorage.getItem('tasks');
+    if(tasks){
+      this.tasks = JSON.parse(tasks)
+    }
+  }
+
   getUserTask (userId:string) {
     return this.tasks.filter((task)=>task.userId === userId)
+    this.saveTasks()
   }
 
   addTask(taskData:CreateTaskType,userId:string) {
@@ -48,10 +56,16 @@ export class TaskService {
       summary:taskData.summary,
       dueDate: taskData.dueDate
     })
+    this.saveTasks();
   }
 
   removeTask (id:string) {
     this.tasks = this.tasks.filter((task)=>task.id !== id)
+    this.saveTasks()
+  }
+
+  private saveTasks () {
+    localStorage.setItem('tasks',JSON.stringify(this.tasks));
   }
 
 }
